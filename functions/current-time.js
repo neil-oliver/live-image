45d25a6cc8f27d4a470e3dde0e2cea3e5865a4ba
neil-oliver <neiloliverphoto@gmail.com>
2024-12-03 15:45:22 -0500

    Update current-time.js

--- functions/current-time.js
+++ functions/current-time.js
@@ -1,13 +1,45 @@
 const sharp = require('sharp');
 
 exports.handler = async (event, context) => {
-    const currentTime = new Date().toLocaleTimeString();
+    // Default timezone and locale
+    const defaultTimeZone = 'UTC';
 
+    // Extract the user's timezone preference from headers
+    const headers = event.headers || {};
+    const timeZone = headers['Time-Zone'] || defaultTimeZone;
+
+    // Get the current time in the user's timezone
+    const now = new Date();
+    const currentTime = new Intl.DateTimeFormat('en-US', {
+        timeZone: timeZone,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hourCycle: 'h23', // 24-hour format
+    }).format(now);
+
+    const [hour, minute, second] = currentTime
+        .split(':')
+        .map((num) => parseInt(num, 10));
+
+    // Generate SVG with bar chart
     const svgImage = `
-        <svg width="300" height="100" xmlns="http://www.w3.org/2000/svg">
-            <rect width="300" height="100" fill="white"/>
-            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24" fill="black">
-                ${currentTime}
+        <svg width="300" height="150" xmlns="http://www.w3.org/2000/svg">
+            <rect width="300" height="150" fill="white"/>
+            <text x="150" y="20" text-anchor="middle" font-size="14" fill="black">
+                Current Time: ${hour}:${minute}:${second}
+            </text>
+            <rect x="50" y="${150 - (hour / 24) * 100}" width="50" height="${(hour / 24) * 100}" fill="#008080" />
+            <text x="75" y="${150 - (hour / 24) * 100 - 5}" text-anchor="middle" font-size="12" fill="black">
+                Hour
+            </text>
+            <rect x="125" y="${150 - (minute / 60) * 100}" width="50" height="${(minute / 60) * 100}" fill="#FFC107" />
+            <text x="150" y="${150 - (minute / 60) * 100 - 5}" text-anchor="middle" font-size="12" fill="black">
+                Minute
+            </text>
+            <rect x="200" y="${150 - (second / 60) * 100}" width="50" height="${(second / 60) * 100}" fill="#6A1B9A" />
+            <text x="225" y="${150 - (second / 60) * 100 - 5}" text-anchor="middle" font-size="12" fill="black">
+                Second
             </text>
         </svg>
     `;
